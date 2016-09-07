f6ef06314ee427aab9cdd7c3f0917b6edec3ee2c
mgniu <mgniu@dataman-inc.com>
2016-09-07 17:39:46 +0800

    add flag parse for server addr

--- src/main.go
+++ src/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/wlshcy/rabbit/src/api"
 )
 
+var (
+	addr = flag.String("addr", "0.0.0.0:8888", "address")
+)
+
 func main() {
+	flag.Parse()
+
 	srv := &http.Server{
-		Addr:           "0.0.0.0:8899",
+		Addr:           *addr,
 		Handler:        api.NewRouter(),
 		MaxHeaderBytes: 1 << 20,
 	}
 
+	log.Printf("[INFO] Server listened on %s", *addr)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("[ERROR] Server shutdown due to %s", err)
 	}
